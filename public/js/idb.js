5213966b6c929a394a37696eea37b5cee0e5d97c
coricathemer <coricathemer@gmail.com>
2021-04-23 17:10:44 -0700

    will post offline changes when back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,7 +10,7 @@ request.onupgradeneeded = function(event) {
   //save a reference to the database
   const db = event.target.result;
   //create an object store (table) called `new-transaction`, set it to have an auto increminting primary key 
-  db.createObjectStore('new-entry', { autoIncrement: true });
+  db.createObjectStore('new_entry', { autoIncrement: true });
 };
 
 // upon successful request
@@ -34,10 +34,10 @@ request.onerror = function(event) {
 // where do I add this functions .catch? index.js?
 function saveRecord(record) {
   // opens a new transaction with the database with read and write permissions
-  const transaction = db.transaction(['new-entry'], 'readwrite');
+  const transaction = db.transaction(['new_entry'], 'readwrite');
 
   // access the object store for 'new-entry'
-  const entryObjectStore = transaction.objectStore('new-entry');
+  const entryObjectStore = transaction.objectStore('new_entry');
 
   // add record to the store with add method
   entryObjectStore.add(record);
@@ -46,19 +46,19 @@ function saveRecord(record) {
 function uploadEntry() {
   alert('Uploaded Entry')
   // open a transaction on the DB
-  const transaction = db.transaction(['new-entry'], 'readwrite');
+  const transaction = db.transaction(['new_entry'], 'readwrite');
 
   // access your object store
-  const entryObjectStore = transaction.objectStore('new-entry');
+  const entryObjectStore = transaction.objectStore('new_entry');
 
   // get all records from store and set to a variable
   const getAll = entryObjectStore.getAll();
 
   // upon a successful .getAll() execution, run this function
-  console.log(JSON.stringify(getAll.result), "I am here ")
+  // console.log(JSON.stringify(getAll.result), "I am here ")
   getAll.onsuccess = function() {
     // if there was data in indexedDB's store, send it to the api server
-    if (getAll.results.length > 0) {
+    if (getAll.result.length > 0) {
       fetch('/api/transaction', {
         method: 'POST', 
         body: JSON.stringify(getAll.result),
@@ -72,9 +72,9 @@ function uploadEntry() {
           throw new Error(serverResponse);
         }
         // open on more transaction
-        const transaction = db.transaction(['new-entry'], 'readwrite');
+        const transaction = db.transaction(['new_entry'], 'readwrite');
         //access the new-entry object stroe
-        const entryObjectStore = transaction.objectStore('new-entry');
+        const entryObjectStore = transaction.objectStore('new_entry');
         // clear all items in the store
         entryObjectStore.clear();
 
